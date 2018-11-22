af4a531461c06a34a87d11ff2e4aaf4d4df9e3ae
min050601 <735567727@qq.com>
2018-11-23 00:53:32 +0800

    Update log.py

--- log.py
+++ log.py
@@ -4,7 +4,7 @@ import os
 
 
 class SpiderLog():
-    def __init__(self,level_str='DEBUG',format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',datefmt='%a, %d %b %Y %H:%M:%S',filename=None,path=None,filemode='w',):
+    def __init__(self,level_str='DEBUG',format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',datefmt='%a, %d %b %Y %H:%M:%S',filename=None,path=None,filemode='a',):
         assert level_str in['DEBUG','INFO','WARNING','ERROR','CRITICAL'],'the log level contains(DEBUG,INFO,WARNING,ERROR,CRITICAL),has not %s'%level_str
         self.format=format
         self.datefmt=datefmt
